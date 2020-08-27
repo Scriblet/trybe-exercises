05e08cf1bed57a3e37ef58f4ab36d83dd6c6f289
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-08-27 18:13:20 -0300

    Resolução exercício 1

---
+++ bloco_4/4.1/exercicio_1.js
@@ -0,0 +1,19 @@
+
+let a = 5;
+
+let b = 8;
+
+let soma,subtracao,multiplicacao,divisao,modulo = 1;
+
+soma = a + b;
+
+subtracao = a - b;
+
+multiplicacao = a * b;
+
+divisao = a / b;
+
+modulo = a % b;
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo);
+
