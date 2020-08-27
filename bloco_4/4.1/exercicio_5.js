0dadf4b06c20fc3552b51af9cf0d4dbd328f5824
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-08-27 19:26:03 -0300

    Resolução exercício 5

---
+++ bloco_4/4.1/exercicio_5.js
@@ -0,0 +1,14 @@
+
+let anguloA = 60;
+let anguloB = 60;
+let anguloC = 60;
+
+if (anguloA + anguloB + anguloC == 180) {
+
+    let triangulo = true;
+    console.log(triangulo);
+} else {
+
+    let triangulo = false;
+    console.log(triangulo);
+}
