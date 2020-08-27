6c0205bb61ab649a3c97ad9604c1b0e5cf56beb2
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-08-27 18:42:52 -0300

    Resolução exercício 3

---
+++ bloco_4/4.1/exercicio_3.js
@@ -0,0 +1,16 @@
+let a = 5;
+
+let b = 8;
+
+let c = 10;
+
+if (a > b && a > c) {
+
+    console.log("O maior número é o a:" , a);
+} else if (b > a && b > c) {
+
+    console.log("O maior número é o b:" , b);
+} else if (c > a && c > b) {
+    
+    console.log("O maior número é o c:" , c);
+}
